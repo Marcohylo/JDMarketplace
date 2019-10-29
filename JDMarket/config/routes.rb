a65cca7c53a2d9721809c5604b7d559910c0f6e7
Marco Lo <marco1993@live.com>
2019-10-29 16:11:44 +1100

    just commiting

--- JDMarket/config/routes.rb
+++ JDMarket/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'store/index'
+  root 'store#index'
   devise_for :users, controllers: {
     registrations: 'registrations'
   }
-  root 'store#index'
+  
   
   # get "/", to: "pages#home", as: "root"
   
