fdc973721402f4009dafd51fe41a0fa9e6ea9f23
Marco Lo <marco1993@live.com>
2019-10-31 16:05:31 +1100

    changes

--- JDMarket/app/models/listing.rb
+++ JDMarket/app/models/listing.rb
@@ -1,3 +1,16 @@
 class Listing < ApplicationRecord
   belongs_to :category
+  belongs_to :user, optional :true
+  mount_uploader :image, ImageUploader
+
+  validates :car_make, :price, :color, :model_year, :engine, :top_speed, :features, :transmission, :condition, :location, :category_id, presence: true 
+
+  validates :description, length: { maximum: 1000, too_long: "%{count} characters is the maximum allowed."}
+  validates :title, length: {maximum: 140, too_long: "%{count} characters is the maximum allowed."}
+  validates :price, numericality: { only_integers: true }
+
+  CAR_MAKE = %w{ Mazda Toyota Mitsubishi Subaru Honda Nissan Suzuki }
+  CONDITION = %w{ New Excellent Mint Used Fair Poor}
+  
+  
 end
--- JDMarket/db/schema.rb
+++ JDMarket/db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< Updated upstream
 ActiveRecord::Schema.define(version: 2019_10_29_045204) do
+=======
+ActiveRecord::Schema.define(version: 2019_10_31_044845) do
+>>>>>>> Stashed changes
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,6 +60,8 @@ ActiveRecord::Schema.define(version: 2019_10_29_045204) do
     t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
+    t.integer "user_id"
     t.index ["category_id"], name: "index_listings_on_category_id"
   end
 
@@ -91,6 +97,11 @@ ActiveRecord::Schema.define(version: 2019_10_29_045204) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+<<<<<<< Updated upstream
+=======
+    t.integer "role"
+    t.json "avatars"
+>>>>>>> Stashed changes
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
