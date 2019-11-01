154a083454674dd50270821d59ad16f35aa5dcca
Marco Lo <marco1993@live.com>
2019-11-01 14:44:13 +1100

    amazoned

--- JDMarket/app/controllers/listings_controller.rb
+++ JDMarket/app/controllers/listings_controller.rb
@@ -106,7 +106,7 @@ class ListingsController < ApplicationController
   
       # Never trust parameters from the scary internet, only allow the white list through.
       def listing_params
-        params.require(:listing).permit(:car_make, :price, :colour, :model_year, :engine, :top_speed, :features, :transmission, :condition, :location,:category_id, :user_id)
+        params.require(:listing).permit(:car_make, :price, :colour, :model_year, :engine, :top_speed, :features, :transmission, :condition, :location,:category_id, :user_id, :picture)
       end
   end
   
--- JDMarket/app/models/listing.rb
+++ JDMarket/app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
   
   before_destroy :not_referenced_by_any_line_item
   belongs_to :category
-
+  has_one_attached :picture
   has_many :line_items
 
   validates :car_make, :price, :colour, :model_year, :engine, :top_speed, :features, :transmission, :condition, :location, :category_id, presence: true 
