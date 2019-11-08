1eef653404c90efd95808ba5d42f041e1cf1f8fd
Marco Lo <marco1993@live.com>
2019-11-08 20:55:48 +1100

    FINAL FINAL PRODUCT

--- JDMarket/app/controllers/application_controller.rb
+++ JDMarket/app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-
+    rescue_from CanCan::AccessDenied do |exception|
+    redirect_to main_app.listings_url, :alert => exception.message
+    end
 end
+
